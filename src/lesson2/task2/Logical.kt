8549fbf39bff6036fc9227662589e2c28176d5d5
danilkushnerov <apple.pacel@gmail.com>
2022-09-16 15:36:24 +0300

    lesson 2 task 2 isNumberHappy, daysInMonth, circleInside

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.pow
 
 /**
  * Пример
@@ -18,7 +19,14 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val firstNumber = (number / 1000).toInt()
+    val secondNumber = (number % 1000 - number % 100) / 100
+    val thirdNumber = (number % 100 - number % 10) / 10
+    val lastNumber = number % 10
+    if (firstNumber + secondNumber == thirdNumber + lastNumber) return true
+    return false
+}
 
 /**
  * Простая (2 балла)
@@ -36,7 +44,31 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int{
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return when (month) {
+            2 -> 29
+            else -> 0
+        }
+    } else {
+        return when (month) {
+            1 -> 31
+            2 -> 28
+            3 -> 31
+            4 -> 30
+            5 -> 31
+            6 -> 30
+            7 -> 31
+            8 -> 31
+            9 -> 30
+            10 -> 31
+            11 -> 30
+            12 -> 31
+            else -> 0
+        }
+    }
+    return 0
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +80,14 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val distSq = kotlin.math.sqrt(((x1 - x2).pow(2)) + ((y1 - y2).pow(2)))
+    return when {
+        distSq + r1 <= r2 -> true
+        else -> false
+    }
+
+}
 
 /**
  * Средняя (3 балла)
