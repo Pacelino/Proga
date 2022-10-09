35b67d05358300ccbc49d08476fdbd9860154d66
danilkushnerov <apple.pacel@gmail.com>
2022-10-09 14:09:56 +0300

    fix days in month

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -50,7 +50,7 @@ fun daysInMonth(month: Int, year: Int): Int{
         return when (month) {
             1, 3, 5, 7, 8, 10, 12 -> 31
             2 -> 28
-            4, 6, 9, 10 -> 30
+            4, 6, 9, 11 -> 30
             else -> 0
         }
     }
