ecb719280e240b0466da0650f51ee863a2e34a1f
danilkushnerov <apple.pacel@gmail.com>
2022-10-21 22:42:14 +0300

    fix2 findSumOfTwo

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -309,12 +309,12 @@ class Tests {
             findSumOfTwo(emptyList(), 1)
         )
         assertEquals(
-            Pair(0, 2),
-            findSumOfTwo(listOf(1, 2, 3), 4)
+            Pair(0, 1),
+            findSumOfTwo(listOf(0, 1, 0), 1)
         )
         assertEquals(
-            Pair(-1, -1),
-            findSumOfTwo(listOf(1, 2, 3), 6)
+            Pair(1, 2),
+            findSumOfTwo(listOf(1, 0, 0), 0)
         )
     }
 
