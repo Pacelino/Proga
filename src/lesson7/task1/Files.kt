8aa5cdb02262396b9cbbf701ce1efb615ff8f5fd
danilkushnerov <apple.pacel@gmail.com>
2022-12-10 13:41:39 +0300

    fixxxx centerFile

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -148,6 +148,7 @@ fun centerFile(inputName: String, outputName: String) {
         for (line in reader) {
             if (reader.size == 1) writer.write(line.trim())
             else {
+                n = maxSize / 2 - line.trim().length / 2
                 writer.write(" ".repeat(maxSize / 2 - line.trim().length / 2))
                 writer.write(line.trim())
                 writer.newLine()
@@ -155,25 +156,7 @@ fun centerFile(inputName: String, outputName: String) {
         }
     }
     writer.close()
-//    if (maxSize == 0) {
-//        writer.write("")
-//        writer.close()
-//    } else if (reader.size == 1) {
-//        reader.forEach { writer.write(it.trim()) }
-//        writer.close()
-//    } else {
-//        for (line in reader) {
-//            if (reader.last() != reader[reader.indexOf(line)] && reader[reader.indexOf(line)] == reader[reader.indexOf(line) + 1]) writer.write(line.trim())
-//            else {
-//                writer.write(" ".repeat(maxSize / 2 - line.trim().length / 2))
-//                writer.write(line.trim())
-//                writer.newLine()
-//            }
-//        }
-//        writer.close()
-//    }
 }
-
 
 /**
  * Сложная (20 баллов)
